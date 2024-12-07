31106a6ee44211e7a79c64d569cfdb18b9aaa1bb
Chackos280 <schacko@binghamton.edu>
2024-12-07 18:28:59 -0500

    Update main.py

--- template_final_project-master/main.py
+++ template_final_project-master/main.py
@@ -6,9 +6,7 @@ from controller import Controller
 
 def main():
     pygame.init()
-    # Create an instance of your controller object
     game_controller = Controller()
-    # Call your mainloop
     game_controller.mainloop()
 
 # https://codefather.tech/blog/if-name-main-python/
